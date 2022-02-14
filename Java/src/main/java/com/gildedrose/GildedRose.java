5ae7b76940b8ca3fe4f691b349f02a3459252124
Simon <simonbesteiro@gmail.com>
2022-02-14 18:16:08 +0100

    refactor

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -22,10 +22,6 @@ class GildedRose {
                     if (item.sellIn < 11) {
                         addQuality(item);
                     }
-
-                    if (item.sellIn < 6) {
-                        addQuality(item);
-                    }
                 }
             }
 
@@ -33,31 +29,29 @@ class GildedRose {
                 item.sellIn = item.sellIn - 1;
             }
 
-            if (item.sellIn < 0) {
-                if (!item.name.equals("Aged Brie")) {
-                    if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                            reduceQuality(item);
-                        }
-                    } else {
-                        item.quality = 0;
-                    }
-                } else {
-                    addQuality(item);
-                }
+
+            if (item.name.equals("Aged Brie") && item.sellIn < 0) {
+                addQuality(item);
             }
+            if (item.name.equals("Backstage passes to a TAFKAL80ETC concert") && item.sellIn < 0) {
+                item.quality = 0;
+            }
+            if (!item.name.equals("Sulfuras, Hand of Ragnaros") && item.sellIn < 0) {
+                reduceQuality(item);
+            }
+
         }
     }
 
     private void reduceQuality(Item item) {
         if (item.quality > 0) {
-            item.quality = item.quality - 1;
+            item.quality--;
         }
     }
 
     private void addQuality(Item item) {
         if (item.quality < 50) {
-            item.quality = item.quality + 1;
+            item.quality++;
         }
     }
 }
