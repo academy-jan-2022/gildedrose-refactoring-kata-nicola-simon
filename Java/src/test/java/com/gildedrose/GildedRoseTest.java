8c0e96963d75e74ec86cdc4f8964a0fa745eef7f
Simon <simonbesteiro@gmail.com>
2022-02-14 16:41:43 +0100

    create first test

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -3,15 +3,17 @@ package com.gildedrose;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class GildedRoseTest {
 
-    @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    @Test void
+    should_lower_quality_sellIn() {
+        Item[] items = new Item[] { new Item("Bread", 20, 30) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        boolean sellInChecker = items[0].sellIn<20;
+        assertTrue(sellInChecker);
     }
 
 }
