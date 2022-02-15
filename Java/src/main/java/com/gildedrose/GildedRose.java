7fed03c110e8991df0d99792dab17ff6991f5e4d
Nicola <nicola_rushton@outlook.com>
2022-02-15 09:36:32 +0000

    refactor

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -9,35 +9,36 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                item.sellIn--;
+
+            if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                continue;
             }
-            if (item.name.equals("Aged Brie")
-                || item.name.equals("Backstage passes to a TAFKAL80ETC concert")
-                || item.name.equals("Sulfuras, Hand of Ragnaros")) {
 
-                if (item.name.equals("Aged Brie")) {
+            item.sellIn--;
+
+            if (item.name.equals("Aged Brie")) {
+                item.quality = addQuality(item.quality);
+
+                if ( item.sellIn < 0) {
                     item.quality = addQuality(item.quality);
-
-                    if ( item.sellIn < 0) {
-                        item.quality = addQuality(item.quality);
-                    }
                 }
-                if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    item.quality = addQuality(item.quality);
+            }
 
-                    if (item.sellIn < 11) {
-                          item.quality = addQuality(item.quality);
-                    }
+            else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                item.quality = addQuality(item.quality);
 
-                    if (item.sellIn < 6) {
-                          item.quality = addQuality(item.quality);
-                    }
-
-                    if (item.sellIn < 0) {
-                        item.quality = 0;
-                    }
+                if (item.sellIn < 11) {
+                      item.quality = addQuality(item.quality);
                 }
+
+                if (item.sellIn < 6) {
+                      item.quality = addQuality(item.quality);
+                }
+
+                if (item.sellIn < 0) {
+                    item.quality = 0;
+                }
+
             }
             else {
                 item.quality = reduceQuality(item.quality);
